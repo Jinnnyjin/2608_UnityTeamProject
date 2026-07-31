806f1d781dfe70a4f39631d866d77e799658f9a6
Wanghyesung <awd5142@naver.com>
2026-07-31 19:16:24 +0900

    [Add] 문자열 키(string) 기반 ObjectPoolManager 및 PoolObject 구현
    -PoolObject 컴포넌트를 기반으로 동작하는 단일 Singleton ObjectPoolManager 작성
    
    -Dictionary<string, Queue<GameObject>> 구조를 사용해 PoolKey 단위로 오브젝트 풀 관리
    
    -Start()에서 초기 풀링 오브젝트 자동 생성 및 비활성화 처리
    
    -GetObject(string) / PushObject(GameObject)를 통한 풀링 입출력 API 제공
    
    -PoolObject 내 자율 수명 관리(m_fAliveTime) 및 Push/Pop 이벤트(OnPush, OnPop) 지원

--- Assets/Wang/Script/ObjectPoolManager.cs
+++ Assets/Wang/Script/ObjectPoolManager.cs
@@ -1,112 +1,107 @@
-//using System;
-//using System.Collections.Generic;
-//using Unity.VisualScripting;
-//using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
-//[Serializable]
-//public class PoolInfo
-//{
-//    public int iPoolCount;
-//    public GameObject refGameObject;
-//}
+[Serializable]
+public class PoolInfo
+{
+    public int iPoolCount;
+    public GameObject refGameObject;
+}
 
-///*///////////////////////////////////////////
-//               ObjectPoolManager
-//기능 : 오브젝트를 미리 로드해두고 필요할 때 꺼내어 쓰면 반납할 수 있게 하는 클래스
-// *///////////////////////////////////////////
+/*///////////////////////////////////////////
+               ObjectPoolManager
+기능 : 오브젝트를 미리 로드해두고 필요할 때 꺼내어 쓰면 반납할 수 있게 하는 클래스
+ *///////////////////////////////////////////
 
-//public class ObjectPoolManager : MonoBehaviour
-//{
-//    public static ObjectPoolManager m_Instance = null;
-//    private Dictionary<PoolObject, Queue<GameObject>> m_hashPool = new Dictionary<PoolObject, Queue<GameObject>>();
+public class ObjectPoolManager : MonoBehaviour
+{
+    public static ObjectPoolManager m_Instance = null;
+    private Dictionary<string, Queue<GameObject>> m_hashPool = new Dictionary<string, Queue<GameObject>>();
 
-//    [SerializeField] private List<PoolInfo> m_listPoolObject = new List<PoolInfo>();
+    [SerializeField] private List<PoolInfo> m_listPoolObject = new List<PoolInfo>();
 
-//    private void Awake()
-//    {
-//        if (m_Instance != null)
-//            Destroy(this);
+    private void Awake()
+    {
+        if (m_Instance != null)
+            Destroy(this);
 
-//        m_Instance = this;
-//        DontDestroyOnLoad(this);
-//    }
+        m_Instance = this;
+        DontDestroyOnLoad(this);
+    }
 
-//    private void Start()
-//    {
-//        for (int i = 0; i < m_listPoolObject.Count; ++i)
-//        {
-//            PoolInfo refPool = m_listPoolObject[i];
-//            PoolObject refPrefabPoolObj = refPool.refGameObject.GetComponent<PoolObject>();
+    private void Start()
+    {
+        for (int i = 0; i < m_listPoolObject.Count; ++i)
+        {
+            PoolInfo refPool = m_listPoolObject[i];
+            PoolObject refPrefabPoolObj = refPool.refGameObject.GetComponent<PoolObject>();
 
-//            if (refPrefabPoolObj == null)
-//            {
-//                Debug.Log("풀 프리팹 미설정 : ObjectPool");
-//                return;
-//            }
+            if (refPrefabPoolObj == null)
+            {
+                Debug.Log("풀 프리팹 미설정 : ObjectPool");
+                return;
+            }
 
-//            Queue<GameObject> queGameObject = new Queue<GameObject>();
-//            m_hashPool.Add(refPrefabPoolObj, queGameObject);
+            Queue<GameObject> queGameObject = new Queue<GameObject>();
+            m_hashPool.Add(refPrefabPoolObj.PoolKey, queGameObject);
 
-//            for (int j = 0; j < refPool.iPoolCount; ++j)
-//            {
-//                GameObject refInstance = Instantiate(refPrefabPoolObj.gameObject);
-//                PoolObject instancePoolObj = refInstance.GetComponent<PoolObject>();
-//                instancePoolObj.SetOriginalPoolObj(refPrefabPoolObj);
-//                PushObject(refInstance);
-//            }
-//        }
-//    }
+            for (int j = 0; j < refPool.iPoolCount; ++j)
+            {
+                GameObject refInstance = Instantiate(refPrefabPoolObj.gameObject);
+                PushObject(refInstance);
+            }
+        }
+    }
 
-//    public GameObject GetObject(PoolObject _refPrefabPoolObj)
-//    {
-//        if (_refPrefabPoolObj == null)
-//            return null;
+    public GameObject GetObject(string _PoolKey)
+    {
+       
+        if (m_hashPool.TryGetValue(_PoolKey, out var queValue) == false)
+            return null;
 
-//        if (m_hashPool.TryGetValue(_refPrefabPoolObj, out var queValue) == false)
-//            return null;
+        if (queValue.Count == 0)
+            return null;
 
-//        if (queValue.Count == 0)
-//            return null;
+        GameObject refObject = queValue.Dequeue();
+        PoolObject PoolObject = refObject.GetComponent<PoolObject>();
+        if (PoolObject == null)
+        {
+            Debug.Log("오브젝트 풀에 이상한 오류 있음");
+            return null;
+        }
 
-//        GameObject refObject = queValue.Dequeue();
-//        IPoolable iPool = refObject.GetComponent<IPoolable>();
-//        if (iPool == null)
-//        {
-//            Debug.Log("오브젝트 풀에 이상한 오류 있음");
-//            return null;
-//        }
+        refObject.transform.SetParent(null);
+        PoolObject.Pop();
+        refObject.gameObject.SetActive(true);
+        return refObject;
+    }
 
-//        refObject.transform.SetParent(null);
-//        iPool.Pop();
-//        refObject.gameObject.SetActive(true);
+    public void PushObject(GameObject _refGameObj)
+    {
+        PoolObject refPoolObj = _refGameObj.GetComponent<PoolObject>();
+        if (refPoolObj == null)
+            return;
 
-//        return refObject;
-//    }
+        if (m_hashPool.TryGetValue(refPoolObj.PoolKey, out var queValue) == false)
+            return;
 
-//    public void PushObject(GameObject _refGameObj)
-//    {
-//        PoolObject refPoolObj = _refGameObj.GetComponent<PoolObject>();
-//        if (refPoolObj == null)
-//            return;
+        if (refPoolObj.PushCount > 0)
+            return;
 
-//        if (m_hashPool.TryGetValue(refPoolObj.PoolKey, out var queValue) == false)
-//            return;
+        refPoolObj.Push();
+        _refGameObj.transform.SetParent(transform);
+        _refGameObj.gameObject.SetActive(false);
 
-//        if (refPoolObj.PushCount > 0)
-//            return;
+        queValue.Enqueue(_refGameObj);
+    }
 
-//        refPoolObj.Push();
-//        _refGameObj.transform.SetParent(transform);
-//        _refGameObj.gameObject.SetActive(false);
+    public int GetObjectCount(string _PoolKey)
+    {
+        if (m_hashPool.TryGetValue(_PoolKey, out var queValue) == false)
+            return -1;
 
-//        queValue.Enqueue(_refGameObj);
-//    }
-
-//    public int GetObjectCount(PoolObject _refPrefabPoolObj)
-//    {
-//        if (m_hashPool.TryGetValue(_refPrefabPoolObj, out var queValue) == false)
-//            return -1;
-
-//        return queValue.Count;
-//    }
-//}
+        return queValue.Count;
+    }
+}
--- Assets/Wang/Script/PoolObject.cs
+++ Assets/Wang/Script/PoolObject.cs
@@ -1,6 +1,62 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+
+//public interface IPoolable
+//{
+//    public string PoolKey { get; }
+//    public int PushCount { get; }
+
+//    public void Push();
+//    public void Pop();
+//}
 
 public class PoolObject : MonoBehaviour
 {
-   
+    [SerializeField] private string m_originalPoolKey;
+
+    private int m_iPushCount = 0;
+    public int PushCount { get { return m_iPushCount; } }
+    public string PoolKey { get { return m_originalPoolKey; } }
+
+
+    public event Action OnPush;
+    public event Action OnPop;
+
+
+    [SerializeField] private float m_fAliveTime = 10.0f;
+    private float m_fSettingAliveTime = 0.0f;
+
+    private void Update()
+    {
+        m_fAliveTime -= Time.deltaTime;
+        if (m_fAliveTime <= 0.0f)
+            ObjectPoolManager.m_Instance.PushObject(gameObject);
+    }
+
+    public virtual void Push()
+    {
+        m_iPushCount = 1;
+        OnPush?.Invoke();
+    }
+
+    public virtual void Pop()
+    {
+        m_iPushCount = 0;
+        m_fAliveTime = m_fSettingAliveTime > 0f ? m_fSettingAliveTime : float.MaxValue;
+        OnPop?.Invoke();
+    }
+
+    public void SetAliveTime(float PushTime)
+    {
+        m_fAliveTime = PushTime;
+        m_fSettingAliveTime = m_fAliveTime;
+    }
+
+    //동적으로 키를 바꿀 이유가 있다면 
+    //public void SetOriginalKey(string _key)
+    //{
+    //    m_originalPoolKey = _key;
+    //}
+
 }
