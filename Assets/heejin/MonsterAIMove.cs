eb94ceaf20dc5271fb2472acd1bf310ecf76cf14
Jinnnyjin <jinnnny.jin@gmail.com>
2026-08-02 01:16:40 +0900

    [add] 몬스터 군중제어 로직 구현
    -  GetSeparateDir() : 주변 몬스터와 겹침 방지, OverlapCircleAll로 반경 내 감지
    - Is Trigger 물리 충돌 및 진동 문제 완화

--- Assets/heejin/MonsterAIMove.cs
+++ Assets/heejin/MonsterAIMove.cs
@@ -2,28 +2,31 @@ using UnityEngine;
 
 public class MonsterAIMove : MonoBehaviour
 {
+    // 주변 타 몬스터와의 반경 
+    [SerializeField] private float m_checkRadius = 1.0f;
+
     private Rigidbody2D m_monsterRb;
+    private Collider2D m_myCollider;
     private Transform m_player;
     private float m_moveSpeed;
 
     private void Awake()
     {
         m_monsterRb = GetComponent<Rigidbody2D>();
+        m_myCollider = GetComponent<Collider2D>();
     }
 
     private void FixedUpdate()
     {
-        if( m_player == null)
-        {
-            return;
-        }
+        if( m_player == null) return;
 
-        Vector2 move = new Vector2(
-            m_player.position.x - m_monsterRb.position.x,
-            m_player.position.y - m_monsterRb.position.y
-            ).normalized;
+        Vector2 chaseDir = GetChaseDIr();
+        Vector2 separateDir = GetSeparateDir();
 
-        m_monsterRb.MovePosition(m_monsterRb.position + move * m_moveSpeed * Time.fixedDeltaTime);
+        Vector2 finalDir = (chaseDir + separateDir).normalized;
+
+        m_monsterRb.MovePosition(m_monsterRb.position + finalDir * m_moveSpeed * Time.fixedDeltaTime);
+
     }
 
     // 생성될때 플레이어 위치와 데이터 받아옴
@@ -35,5 +38,37 @@ public class MonsterAIMove : MonoBehaviour
     public void SetData(MonsterData _data)
     {
         m_moveSpeed = _data.MoveSpeed;
+    }
+
+    private Vector2 GetSeparateDir()
+    {
+        Vector2 separation = Vector2.zero;
+
+        // 주변 타 몬스터와 겹치지 않도록
+        Collider2D[] nearbyMonsters = Physics2D.OverlapCircleAll(transform.position, m_checkRadius);
+
+        // 자신 제외
+        foreach (Collider2D other in nearbyMonsters)
+        {
+            if (other == m_myCollider)
+            {
+                continue;
+            }
+
+            separation += new Vector2
+                (m_monsterRb.position.x - other.transform.position.x,
+                m_monsterRb.position.y - other.transform.position.y);
+        }
+
+        return separation.normalized;
+    }
+
+    private Vector2 GetChaseDIr()
+    {
+        Vector2 move = new Vector2
+            (m_player.position.x - m_monsterRb.position.x,
+            m_player.position.y - m_monsterRb.position.y).normalized;
+
+        return move;
     }
 }
