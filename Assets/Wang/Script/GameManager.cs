c7563b4a8ccdd8f7dfcb31703b0282c50e52f686
Wanghyesung <awd5142@naver.com>
2026-08-04 12:47:11 +0900

    [Add] GameManager 초기 개발
    -EXP, HP 슬라이더와 연동하여 현재 체력을 표시하는 방향
    
    -EXP가 다 차면 레벨을 하고 랜덤 기능을 선택하는 방식

--- Assets/Wang/Script/GameManager.cs
+++ Assets/Wang/Script/GameManager.cs
@@ -1,11 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager m_Instance { get; private set; }
 
+    [SerializeField] private Slider m_expSlider;
+    [SerializeField] private Slider m_hPSlider;
+
+    [SerializeField] private float m_currentExp = 0.0f;
+    [SerializeField] private float m_maxExp = 0.0f;
+    private float m_currentHP = 0.0f;
+    [SerializeField] private float m_maxHP = 0.0f;
+    private int m_iCurrentLevel = 0;
+
+    //이건 플레이어 쪽에서 만들어주셨음 좋겠어요
+    public event Action<float> m_onDamaged; //현제 내가 받은 데이미 양
+    public event Action<float> m_onRecvExp; //현제 내가 받은 경험치 양
+
     private void Awake()
     {
+        
         if (m_Instance != null && m_Instance != this)
         {
             Destroy(gameObject);
@@ -17,7 +33,32 @@ public class GameManager : MonoBehaviour
     [SerializeField] private PlayerMovement m_Player;
     public PlayerMovement Player => m_Player;
 
-    
+    private void AddExp(float _amount)
+    {
+        if (_amount <= 0)
+            return;
+
+        m_currentExp += _amount;
+        if(m_currentExp <= m_maxExp)
+        {
+            m_currentExp -= m_maxExp;
+            LevelUp();
+        }
+    }
+
+    // ExSlider의 채우기 연출이 실제로 Max에 도달했을 때 Player가 호출
+    private void LevelUp()
+    {
+        m_iCurrentLevel += 1;
+
+        //OnLevelUp?.Invoke(m_iCurrentLevel);
+        //m_refCardCreator?.ShowChoices();
+    }
+
+    private void AddHP()
+    {
+
+    }
 
     //나중에 콜백으로 연결해두기
     public void EndGame()
