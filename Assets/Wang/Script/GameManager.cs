579370988c43d900d2bc25b1334bc102026b7dca
Wanghyesung <awd5142@naver.com>
2026-08-04 13:35:54 +0900

    [Add] 게임 매니저 데미지 계산 추가

--- Assets/Wang/Script/GameManager.cs
+++ Assets/Wang/Script/GameManager.cs
@@ -15,13 +15,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float m_maxHP = 0.0f;
     private int m_iCurrentLevel = 0;
 
-    //이건 플레이어 쪽에서 만들어주셨음 좋겠어요
-    public event Action<float> m_onDamaged; //현제 내가 받은 데이미 양
-    public event Action<float> m_onRecvExp; //현제 내가 받은 경험치 양
-
     private void Awake()
     {
-        
         if (m_Instance != null && m_Instance != this)
         {
             Destroy(gameObject);
@@ -33,17 +28,25 @@ public class GameManager : MonoBehaviour
     [SerializeField] private PlayerMovement m_Player;
     public PlayerMovement Player => m_Player;
 
+    private void Start()
+    {
+        //플레이어 Dead이벤트 구독
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        m_currentHP -= _damage;
+        if (m_currentHP <= 0)
+        {
+            m_currentHP = 0;
+            //플레이어 사망 처리
+        }
+        m_hPSlider.value = m_currentHP / m_maxHP;
+    }
+
     private void AddExp(float _amount)
     {
-        if (_amount <= 0)
-            return;
-
-        m_currentExp += _amount;
-        if(m_currentExp <= m_maxExp)
-        {
-            m_currentExp -= m_maxExp;
-            LevelUp();
-        }
+        
     }
 
     // ExSlider의 채우기 연출이 실제로 Max에 도달했을 때 Player가 호출
